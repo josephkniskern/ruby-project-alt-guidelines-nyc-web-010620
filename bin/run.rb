ac12a8e22a6f4ac444aa95c46d591cdf7c355543
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-01-21 18:10:59 -0500

    done

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,61 @@
 require_relative '../config/environment'
+require 'tty-prompt'
 
-puts "hello world"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# prompt = TTY::Prompt.new
+# prompt.ask("What is your name?")
+# prompt.select("What would you like to do?", %w(All_listings My_listings))
+
+# system("clear")
+
+# def prompt_user
+#     system("clear")
+#     puts "Hello! Please enter your name?"
+#     name = gets 
+#     puts "Welcome #{name}! What would you like to do today?"
+#     puts ""
+# end
+
+# prompt_user
+
+#     user_input = gets.chomp.to_i
+#     action_router(user_input)
+# end
+
+# def action_router(input)
+#     case input 
+#     when 1
+#         create_buyer
+#     end
+#     gets.chomp
+#     prompt_user
+# end
+
+# def create_buyer
+#     Buyer.create(:name)
+# end
+
+
+
+
