2527c1f2350bd1609b2637096758f161d4298e80
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-01-22 10:09:01 -0500

    new commit

--- bin/run.rb
+++ bin/run.rb
@@ -3,14 +3,61 @@ require 'tty-prompt'
 
 prompt = TTY::Prompt.new
 name = prompt.ask("What is your name?")
+prompt.say("hello! #{name}")
+#Buyer.new =  
 
-Buyer.all.each do |buyer|
-    puts buyer.name
+choice = prompt.select("What would you like to do today?") do |menu|
+    menu.choice name: "Check all listings?"
+    menu.choice name: "Check favorites?"
 end
+
+
+
+
+if choice == "Check all listings?"
+    question1 = "Choose a listing."
+    listing_choice = Listing.all.each { |listing| puts listing} 
+    all_listings = prompt.select(question1, listing_choice)
+    add_choice = prompt.select("Add?") do |menu|
+        menu.choice name: "Add to favorites?"
+        menu.choice name: "Go back"
+    end
+    if add_choice == "Add to favorites?"
+        #Favorite.all << 
+        
+
+    elsif add_choice == "Go back"
+          
+        
+    end
+        
+
+
+elsif choice == "Check favorites?"
+    question2 = "See your favorites?"
+    favorite_choice = Favorite.all.each { |favorite| puts favorite.buyer_id ==buyer.id}
+    all_favorites = prompt.select(question2, favorite_choice)
+    add_fave_choice = prompt.select()
+
+else 
+    puts "NO"
+end
+
+
+
+end
+
+
+
+
+
+
+
+
 # Buyer.create(name: name)
 # prompt.select("What would you like to do?", %w(All_listings My_listings))
 
-binding.pry
+#binding.pry
 
 
 
